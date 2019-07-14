d0105c02f50755561e5081fc016ad5aefb1199df
johanesr <johanesronaldo@gmail.com>
2019-07-14 16:48:55 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {PureComponent} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends PureComponent{
+  state = {
+    isMobile: false
+  };
+
+  componentDidMount() {
+    //Determine if the App is mobile or web
+    this.setState({
+      isMobile: /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;

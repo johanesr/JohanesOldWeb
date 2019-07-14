50fb51b963d87005752090459f1e395cba4bbfc6
johanesr <johanesronaldo@gmail.com>
2019-07-14 17:15:15 +0700

    isMobile Test

--- src/App.js
+++ src/App.js
@@ -15,11 +15,13 @@ class App extends PureComponent{
   }
 
   render() {
+    const {isMobile} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            {!isMobile ? "Web Display" : "Mobile Display"}
             Edit <code>src/App.js</code> and save to reload.(Test Rule)
           </p>
           <a
